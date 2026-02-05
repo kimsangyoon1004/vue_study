5338730d234b392ee1ec69a36aba9c51ab8f4a64
kimsangyoon1004 <eric_yoon1@naver.com>
2026-02-05 19:43:44 +0900

    dyname_ref

---
+++ dynamic_ref/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+
+const titleClass = ref('');
+
+const good = () => {
+  titleClass.value = 'title'
+}
+</script>
+
+<template>
+  <h1 :class ="titleClass"> 빨갛게 만들기 </h1>
+  <button @click="good"> 클릭 </button>
+</template>
+
+<style scoped>
+.title {color:red}
+</style>
