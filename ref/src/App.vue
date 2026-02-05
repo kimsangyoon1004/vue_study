ae9fd1702660c42927d4209bf27c17188424c02e
kimsangyoon1004 <eric_yoon1@naver.com>
2026-02-05 19:27:26 +0900

    ref

---
+++ ref/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import {ref, computed, reactive} from "vue";
+const cnt = ref(0);
+const oddEven = computed(() => (cnt.value % 2) ? '홀수' : '짝수');
+const counter = () => cnt.value++;
+
+const obj1 = reactive({
+  name: "kim", age: 33
+});
+
+const good = () => {
+  cnt.value++
+  obj1.name = "lee"
+}
+</script>
+
+<template>
+  <p> {{ cnt }}</p>
+  <p>홀짝 판별 : {{ oddEven }}</p>
+  <button @click="counter">버튼 클릭</button>
+  <p>obj1 : {{  obj1.name }} / {{ obj1.age }}</p>
+  <button @click="good"> 카운터 클릭</button>
+</template>
+
+<style scoped></style>
