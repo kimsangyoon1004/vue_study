bb44fa7a55d7f4c2b46f4b8fcc8002a6ec978fb6
kimsangyoon1004 <eric_yoon1@naver.com>
2026-02-05 19:47:34 +0900

    computed

---
+++ computed/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {ref, computed} from 'vue';
+const cnt = ref(10);
+const oddEven = computed( () => cnt.value % 2 ? '홀수' : '짝수');
+
+const counter = () =>{
+  cnt.value++;
+}
+</script>
+
+<template>
+  <p> cnt: {{ cnt }}</p>
+  <p>홀짝 판별 : {{ oddEven }}</p>
+  <button @click="counter"> 클릭 </button>
+</template>
+
+<style scoped></style>
