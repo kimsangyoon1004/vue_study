b674dd8f6ba83135741704d1a7a860b79fe9071f
kimsangyoon1004 <eric_yoon1@naver.com>
2026-02-07 17:56:03 +0900

    watch

---
+++ watch/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref, reactive, watch } from 'vue';
+
+const cnt = ref(1)
+const obj = reactive({oldV: null, newV:null})
+const counter = () => cnt.value++
+
+watch(cnt, (newValue, oldValue) => {
+  obj.oldV = oldValue;
+  obj.newV = newValue;
+})
+</script>
+
+<template>
+  <p>cnt: {{ cnt }}</p>
+  <p>old: {{ obj.oldV }} / new: {{ obj.newV }}</p>
+  <button @click="counter">클릭</button>
+</template>
+
+<style scoped></style>
---
+++ watch_immediate/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref,  watch } from 'vue';
+
+const count = ref(0)
+const counter = () => count.value++
+const obj = ref({name: "홍길동", age: 20})
+const updateName =() => obj.value.name = "김길동"
+watch(count, (newV, oldV) => {
+  console.log(`${oldV} => ${newV}`)
+}, { immediate: true })
+
+watch(obj, (newobj) => {
+  console.log(newobj.name)
+}, { deep: true })
+</script>
+
+<template>
+  <p>count: {{ count }}</p>
+  <button @click="counter">클릭</button>
+
+  <h1> Profile</h1>
+  <p>name: {{ obj.name }}</p>
+  <p>age: {{ obj.age }}</p>
+  <button @click="updateName">이름 변경</button>
+</template>
+
+<style scoped></style>
