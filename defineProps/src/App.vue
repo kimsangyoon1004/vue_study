14dabb07ed6159c7ca843560ce5ae6a42869bb75
kimsangyoon1004 <eric_yoon1@naver.com>
2026-02-07 19:31:16 +0900

    defineProps

---
+++ defineProps/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import ButtonCounter from './ButtonCounter.vue';
+
+//const post = {
+//  id: 1,
+//  title: 'Hello World',
+//  mainCont: 'This is a test post',
+//} 
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  mainCont: {
+    type: String,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <h1> 아래 있는 컴포넌트는 자식 컴포넌트</h1>
+  <ButtonCounter :title="post.title" :mainCont="post.mainCont" />
+</template>
+
---
+++ defineProps/src/ButtonCounter.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { onMounted,ref } from 'vue';
+const props = defineProps(['title', 'mainCont'])
+const title = ref('')
+const cont = ref('')
+
+onMounted(() => {
+    title.value = props.title
+    cont.value = props.mainCont
+})
+</script>
+
+<template>
+    <h1> title : {{ title }} </h1>
+    <p>  cont: {{ cont }}</p>
+</template>
