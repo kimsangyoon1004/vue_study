ac160f6f99fb2a96ed900aaf5046d8fc97de893e
kimsangyoon1004 <eric_yoon1@naver.com>
2026-02-08 17:08:05 +0900

    emits

---
+++ defineEmits/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import ChildComponent from './ChildComponent.vue';
+
+const handleCustomEvent = (message) => {
+  console.log(message)
+}
+</script>
+
+<template>
+  <ChildComponent @customEvents="handleCustomEvent" />
+</template>
+
+<style scoped></style>
---
+++ defineEmits/src/ChildComponent.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { defineEmits } from 'vue';
+const emit = defineEmits(['customEvent'])
+const handleClick = () => {
+    emit('customEvent', 'Button Clicked')
+}
+</script>
+
+<template>
+    <button @click = "handleClick">Click me</button>
+</template>
